222b2e314a156b85a505ae9b9ab7e32e0830fb32
affi1504 <afrid.afnan71@gmail.com>
2020-12-07 11:43:33 +0530

    login page

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 include('public\components\header.php'); 
 ?>
 
-
+<body>
+</body>
 
 <?php
 include('public\components\footer.php');
-
 ?>
--- public/components/header.php
+++ public/components/header.php
@@ -8,6 +8,5 @@
 </head>
 <header>
 </header>
-<body>
 
     
